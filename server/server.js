c0229b6c6ee06ea7eb49ad54fed5f463eba26d5d
flysking <flysking@naver.com>
2023-12-04 00:22:51 +0900

    서버수정

--- server/server.js
+++ server/server.js
@@ -593,7 +593,7 @@ app.get('/BoardList/:category', (req, res) => {
 app.get('/BoardListNoticeByCate/:category', (req, res) => {
   //게시글 목록 조회
   const category=req.params.category;
-  BoardDAO.BoardListNotice(category,(error, boards) => {
+  BoardDAO.BoardListNoticeByCate(category,(error, boards) => {
     if (error) {
       console.error(error);
       res.status(500).json({success: false});
@@ -606,7 +606,7 @@ app.get('/BoardListNoticeByCate/:category', (req, res) => {
 app.get('/BoardListPopularByCate/:category', (req, res) => {
   //게시글 목록 조회
   const category=req.params.category;
-  BoardDAO.BoardListPopular(category,(error, boards) => {
+  BoardDAO.BoardListPopularByCate(category,(error, boards) => {
     if (error) {
       console.error(error);
       res.status(500).json({success: false});
